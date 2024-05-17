47ab083c032f152e14cf57661f2c5f572ee014d7
Facundo <topafacundo@gmail.com>
2024-05-16 22:59:55 -0300

    Update cliente.c
    Actualización del archivo cliente.c

--- Hilos/cliente.c
+++ Hilos/cliente.c
@@ -9,12 +9,13 @@
 #define PORT 8080
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s <server_ip>\n", argv[0]);
+    if (argc != 3) {
+        printf("Ingreso: %s <server_ip> <priority>\n", argv[0]);
         return 1;
     }
 
     char *server_ip = argv[1];
+    int priority = atoi(argv[2]);
 
     int client_socket;
     struct sockaddr_in server_address;
@@ -22,7 +23,7 @@ int main(int argc, char *argv[]) {
 
     // Crear el socket
     if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("Socket creation error");
+        perror("Creacion erronea de Socket");
         exit(EXIT_FAILURE);
     }
 
@@ -32,29 +33,50 @@ int main(int argc, char *argv[]) {
 
     // Convertir dirección IP de texto a binario
     if (inet_pton(AF_INET, server_ip, &server_address.sin_addr) <= 0) {
-        perror("Invalid address/ Address not supported");
+        perror("Direccion incorrecta o no soportoda");
         exit(EXIT_FAILURE);
     }
 
     // Conectar al servidor
     if (connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
-        perror("Connection failed");
+        perror("Conexion fallida");
         exit(EXIT_FAILURE);
     }
 
-    printf("Connected to server\n");
+    printf("Conectado al servidor\n");
+
+    // Enviar la prioridad al servidor
+    char priority_buffer[10];
+    sprintf(priority_buffer, "%d", priority);
+    if (send(client_socket, priority_buffer, strlen(priority_buffer), 0) < 0) {
+        perror("Envio fallido");
+        close(client_socket);
+        exit(EXIT_FAILURE);
+    }
 
     // Mantener la comunicación con el servidor
     while (1) {
-        printf("Enter message: ");
+        printf("Ingrese el mensaje: ");
         fgets(buffer, sizeof(buffer), stdin);
 
         // Enviar el mensaje al servidor
-        send(client_socket, buffer, strlen(buffer), 0);
+        if (send(client_socket, buffer, strlen(buffer), 0) < 0) {
+            perror("Envio fallido");
+            break;
+        }
 
         // Leer la respuesta del servidor
-        int valread = read(client_socket, buffer, sizeof(buffer));
-        printf("Server response: %s\n", buffer);
+        int valread = read(client_socket, buffer, sizeof(buffer) - 1);
+        if (valread < 0) {
+            perror("Lectura fallida");
+            break;
+        } else if (valread == 0) {
+            printf("Server desconectado\n");
+            break;
+        }
+
+        buffer[valread] = '\0';
+        printf("Respuesta de Server: %s\n", buffer);
     }
 
     // Cerrar el socket
