9540e86dfb0fcfadc0938d37b96baed280caa082
Facundo <topafacundo@gmail.com>
2024-05-24 18:52:43 -0300

    Update clienteApache.c

--- Apache/clienteApache.c
+++ Apache/clienteApache.c
@@ -8,40 +8,63 @@
 #include <unistd.h>
 #define IP "192.168.0.134"
 #define PUERTO 80
-int main (int argc, char *argv[])
- {
-   char file_name[256] = "index.html\0";
-   char buf[8192];
-   char message[8192];
-   int sd;
-   struct sockaddr_in pin;
-   pin.sin_family = AF_INET;
-   pin.sin_addr.s_addr = inet_addr(IP);
-   pin.sin_port = htons(PUERTO);
-   bzero(&pin.sin_zero, sizeof(pin.sin_zero));
-   if ((sd=socket(AF_INET,SOCK_STREAM,0)) == -1)
-        {
-                printf("Error al abrir el socket\n");
-                exit(1);
-      }
-    if(connect(sd, (void *)&pin,sizeof(pin)) == -1)
-        {
-                printf("Error al conectar el socket\n");
-                exit(1);
-       }
-      sprintf(message, "GET /%s \n",file_name);
-      if(send(sd,message,strlen(message),0) == -1)
-        {
-                printf("Error al enviar");
-                exit(1);
+
+void eliminarEtiquetasHTML(char *source, char *dest) {
+    int in_tag = 0;
+    while (*source) {
+        if (*source == '<') {
+            in_tag = 1;
+        } else if (*source == '>') {
+            in_tag = 0;
+        } else if (!in_tag) {
+            *dest++ = *source;
         }
-        printf("mensaje %s enviado al servidor apache...\n",message);
-        if(recv(sd,buf,8192,0) == -1)
-        {
-                printf("Error al recibir la repuesta..\n");
-                exit(1);
-        }
-        printf("Respuesta del servidor: \n%s\n",buf);
-        close(sd);
-        return EXIT_SUCCESS;
+        source++;
+    }
+    *dest = '\0';
+}
+
+int main(int argc, char *argv[]) {
+    char file_name[256] = "index.html";
+    char buf[8192];
+    char message[8192];
+    char plain_text[8192];
+    int sd;
+    struct sockaddr_in pin;
+    pin.sin_family = AF_INET;
+    pin.sin_addr.s_addr = inet_addr(IP);
+    pin.sin_port = htons(PUERTO);
+    bzero(&pin.sin_zero, sizeof(pin.sin_zero));
+
+    if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        printf("Error al abrir el socket\n");
+        exit(1);
+    }
+
+    if (connect(sd, (void *)&pin, sizeof(pin)) == -1) {
+        printf("Error al conectar el socket\n");
+        exit(1);
+    }
+
+    sprintf(message, "GET /%s HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n", file_name, IP);
+
+    if (send(sd, message, strlen(message), 0) == -1) {
+        printf("Error al enviar\n");
+        exit(1);
+    }
+
+    printf("Mensaje %s enviado al servidor apache...\n", message);
+
+    if (recv(sd, buf, 8192, 0) == -1) {
+        printf("Error al recibir la respuesta\n");
+        exit(1);
+    }
+
+    printf("Respuesta del servidor: \n%s\n", buf);
+
+    eliminarEtiquetasHTML(buf, plain_text);
+    printf("Contenido sin etiquetas HTML: \n%s\n", plain_text);
+
+    close(sd);
+    return EXIT_SUCCESS;
 }
