03e4b3067cf8956784a4d9b3e61ddaa569883363
Facundo <topafacundo@gmail.com>
2024-06-17 16:25:07 -0300

    Update proceso1.c

--- 07 - Sincronizacion Distribuida/01 - Lamport/proceso1.c
+++ 07 - Sincronizacion Distribuida/01 - Lamport/proceso1.c
@@ -7,11 +7,11 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <netdb.h>
-#include <sys/stat.h> 
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <time.h>
 
-#define IP "192.168.0.209" //Ubicacion 
+#define IP "192.168.0.215" //Ubicacion
 #define PUERTO 8888
 
 void UbicacionDelCliente(struct sockaddr_in);
@@ -37,26 +37,26 @@ int main(int argc, char *argv[])
    printf("listen %d\n",listen(idsocks,5));
 
    while(1)
-   {  
-     printf("esperando conexion\n");
+   {
+     printf("Esperando conexion\n");
      idsockc = accept(idsocks,(struct sockaddr *)&c_sock,&lensock);
 
      if(idsockc != -1)
      {
-         if (!fork()) 
-             {  
-               /* Ubicacion del Cliente */    
-               printf("conexion aceptada desde el cliente\n");
+         if (!fork())
+             {
+               /* Ubicacion del Cliente */
+               printf("Conexion aceptada desde el cliente\n");
                //UbicacionDelCliente(c_sock);
-               /*--------------------------------------------------*/  
+               /*--------------------------------------------------*/
                RecibeEnviaMensaje(idsockc);
-	       close(idsockc);
-	       exit(0);    
-             }   
+               close(idsockc);
+               exit(0);
+             }
      }
      else
      {
-        printf("Servidor --> conexion rechazada %d \n",idsockc);
+        printf("Conexion rechazada %d \n",idsockc);
      }
    }
 }
@@ -67,59 +67,59 @@ void RecibeEnviaMensaje(int idsockc)
     int nb;
     int defout ;
     nb = read(idsockc,buf,30);
-    
+
     buf[nb] = '\0';
     printf("Hora de envio del mensaje A: %s\n" ,buf);
-    
+
     char hora[128];
     printf("\nHora de recepcion del mensaje A: ");
-	
+
+    sleep(10);
+
     DameHoraMaquina(hora);
-    	
+
     printf("%s\n", hora);
 
     int hr, min, seg;
-    int hr2, min2, seg2;	
-    hr = atoi(strtok(buf, ":"));		
+    int hr2, min2, seg2;
+    hr = atoi(strtok(buf, ":"));
     min= atoi(strtok(NULL, ":"));
     seg= atoi(strtok(NULL, ":"));
 
-    hr2 = atoi( strtok(hora, ":"));		
+    hr2 = atoi( strtok(hora, ":"));
     min2= atoi(strtok(NULL, ":"));
     seg2= atoi(strtok(NULL, ":"));
 
-    
+
     if(hr>hr2)
     {
-      hr2=hr+1;	
+      hr2=hr+1;
       printf("\nCorregir");
-      printf("\n Nueva hora %d:%d: %d", hr2 , min2 , seg2 );
+      printf("\n Nueva hora %02d:%02d:%02d \n", hr2 , min2 , seg2 );
     }
     else
       if(hr==hr2)
-       	{
- 	  if(min>min2)
-	  {
-	    min2=min+1;
-	    printf("\nCorregir");
-	    printf("\n Nueva hora %d:%d: %d", hr2 , min2 , seg2 );			
-    	  }
- 	  else
-	    if(min==min2)	
-    	    {
-		if(seg>seg2 || seg==seg2)
-		{
-		  seg2=seg+1;
-		  printf("\nCorregir");
-		  printf("\n Nueva hora %d:%d: %d", hr2 , min2 , seg2 );
-		}
-	    }
-	}
+        {
+          if(min>min2)
+          {
+            min2=min+1;
+            printf("\nCorregir");
+            printf("\n Nueva hora %02d:%02d:%02d \n", hr2 , min2 , seg2 );
+          }
+          else
+            if(min==min2)
+            {
+                if(seg>seg2 || seg==seg2)
+                {
+                  seg2=seg+1;
+                  printf("\nCorregir");
+                  printf("\n Nueva hora %02d:%02d:%02d \n", hr2 , min2 , seg2 );
+                }
+            }
+        }
 
-     //Sleep(3000);	
- 
      enviarMensajeB();
-  
+
 }
 
 
@@ -136,29 +136,28 @@ void DameHoraMaquina(char * Hora)
   struct tm * tlocal= localtime(&tiempo);
   strftime(Hora,128, "%H:%M:%S", tlocal);
 }
-  
+
 int enviarMensajeB()
 {
-   int sd; 
-   struct sockaddr_in pin; 
+   int sd;
+   struct sockaddr_in pin;
 
-   pin.sin_family = AF_INET; 
-   pin.sin_addr.s_addr = inet_addr("192.168.0.15");
-   pin.sin_port = htons(9999); 
-   bzero(&pin.sin_zero, sizeof(pin.sin_zero)); 
+   pin.sin_family = AF_INET;
+   pin.sin_addr.s_addr = inet_addr("192.168.0.215");
+   pin.sin_port = htons(9999);
+   bzero(&pin.sin_zero, sizeof(pin.sin_zero));
 
    if ((sd = socket(AF_INET,SOCK_STREAM,0)) == -1)  return -1;
    if (connect(sd,(void *)&pin,sizeof(pin)) == -1)  return -2;
-    
-   //Enviando mensaje B	
+
+   //Enviando mensaje B
    char bufin[512] ;
-   printf("\n Hora de envio del mensaje B: ");
+   printf("\nHora de envio del mensaje B: \n");
    memset(bufin,0,512);
    DameHoraMaquina(bufin);
    printf("%s\n", bufin);
    send(sd,bufin, strlen(bufin),0) ;
-   close(sd);   
-   return 0 ;	
+   close(sd);
+   return 0 ;
 
 }
-
